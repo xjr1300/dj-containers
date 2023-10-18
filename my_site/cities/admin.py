3b49d8e540258eab74c2a8ae835bf75f541d2dd5
Yasunori Ishiguro <xjr1300.04@gmail.com>
2023-10-18 14:46:51 +0900

    Register a city model to admin site.

--- my_site/cities/admin.py
+++ my_site/cities/admin.py
@@ -1,1 +1,16 @@
-# Register your models here.
+from django.contrib import admin
+from django.contrib.gis.admin import OSMGeoAdmin
+
+from .models import City
+
+
+class CityAdmin(OSMGeoAdmin):
+    fields = ("code", "name", "geom")
+    list_display = ("code", "name")
+    ordering = ("code",)
+    search_fields = (
+        "code",
+        "name",
+    )
+
+admin.site.register(City, CityAdmin)
