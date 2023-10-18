3db1de8ecd8c4bb8c0e9614c9e0dfcc642f77e92
Yasunori Ishiguro <xjr1300.04@gmail.com>
2023-10-18 14:42:48 +0900

    Implement a city model.

--- my_site/cities/models.py
+++ my_site/cities/models.py
@@ -1,2 +1,30 @@
+from typing import ClassVar, Self
 
-# Create your models here.
+from django.contrib.gis.db import models as geo_models
+
+
+class City(geo_models.Model):
+    """市区町村モデル"""
+
+    code = geo_models.CharField("市区町村コード", max_length=5, primary_key=True)
+    name = geo_models.CharField("市区町村名", max_length=20)
+    geom = geo_models.MultiPolygonField("ジオメトリ", srid=4326)
+
+    class Meta:
+        verbose_name: ClassVar[str] = "市区町村"
+        verbose_name_plural: ClassVar[str] = "市区町村"
+        ordering: ClassVar[[list[str]]] = ["code"]
+
+    def __str__(self: Self) -> str:
+        """
+        市区町村名を返却する。
+
+        Args:
+        ----
+            self (Self): モデルインスタンス
+
+        Returns:
+        -------
+            str: 市区町村名
+        """
+        return self.name
