5c76bb44e82b2a8b02c1d32e937ffa2756d87345
Ishiguro Yasunori <xjr1300.04@gmail.com>
2023-10-18 11:42:56 +0900

    Register a prefecture model to admin site.

--- my_site/prefectures/admin.py
+++ my_site/prefectures/admin.py
@@ -1,1 +1,18 @@
-# Register your models here.
+from django.contrib import admin
+from django.contrib.gis.admin import OSMGeoAdmin
+
+from .models import Prefecture
+
+
+class PrefectureAdmin(OSMGeoAdmin):
+    fields = ("code", "name")
+    list_display = ("code", "name")
+    ordering = ("code",)
+    list_display_links = ("code",)
+    search_fields = (
+        "code",
+        "name",
+    )
+
+
+admin.site.register(Prefecture, PrefectureAdmin)
